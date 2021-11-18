129c2c8ea5754891956b4121bfc602bc6f536c1d
IanWach <71596158+IanWach@users.noreply.github.com>
2021-11-18 18:17:02 +0300

    File
    Additional Files

--- SimpleValidationForm/Form-Validation/function.js
+++ SimpleValidationForm/Form-Validation/function.js
@@ -1,11 +1,19 @@
 var click = document.getElementById("clicked")
 var imgclick = document.getElementsByClassName("img-resp")
 var unclick = document.getElementById("clicked")
+var navclick = document.getElementById("nav-click")
 function para(){
         click.style.display="block";
-        click.style.overflow = "hidden";
+        click.style.overflow = "scroll";
         imgclick.style.width ="150px";
 }
 function para2(){
          unclick.style.display ='none'
+}
+function shownav(){
+        navclick.style.display='block'
+        
+}
+function hidenav(){
+              navclick.style.display ='none'
 }
